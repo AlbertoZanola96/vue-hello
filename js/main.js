3d61e12d39aa00d38f2e4a1c4d6ac04f5a698c26
alberto <albertozanola1996@gmail.com>
2021-09-06 15:18:58 +0200

    Stampa a schermo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Vue.config.devtolls = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            saluto: 'hello world'
+        }
+    }
+)
